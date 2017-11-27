9ca98919ea0806d222de94e222ab9f9645944f63
brian <brian@zipcoders-MacBook-Pro.local>
2017-11-27 11:17:54 -0500

    First 30 mins

--- src/main/java/io/zipcoder/Problem2.java
+++ src/main/java/io/zipcoder/Problem2.java
@@ -1,4 +1,43 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Problem2 {
+
+    public String fibonacciIteration(Integer n) {
+        ArrayList<Integer> fibSequence = new ArrayList<>();
+        fibSequence.add(0);
+        fibSequence.add(1);
+        Integer lowerIndex = 0;
+        Integer higherIndex = 1;
+        Integer nextFibNumber;
+
+        do {
+            nextFibNumber = fibSequence.get(lowerIndex) + fibSequence.get(higherIndex);
+            if (nextFibNumber > n) {
+                break;
+            }
+            fibSequence.add(nextFibNumber);
+            lowerIndex++;
+            higherIndex++;
+
+        } while(true);
+
+        return fibSequence.toString();
+    }
+
+    public String fibonacciRecursion(Integer n) {
+        ArrayList<Integer> newFibSequence = new ArrayList<>();
+        newFibSequence.add(0);
+        newFibSequence.add(1);
+
+        Integer sizeOfList = newFibSequence.size();
+        Integer nextFibNumber = newFibSequence.get(sizeOfList - 2) + newFibSequence.get(sizeOfList - 1);
+
+
+        newFibSequence.add(nextFibNumber);
+
+
+        return newFibSequence.toString();
+    }
 }
--- src/test/java/io/zipcoder/Problem2Test.java
+++ src/test/java/io/zipcoder/Problem2Test.java
@@ -1,4 +1,26 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem2Test {
+
+    @Test
+    public void fibonacciIterationTest() {
+        String actual = new Problem2().fibonacciIteration(150);
+        String expected = "[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]";
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void fibonacciRecursionTest() {
+        String actual = new Problem2().fibonacciRecursion(150);
+        String expected = "[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]";
+
+        Assert.assertEquals(expected, actual);
+
+
+    }
 }
