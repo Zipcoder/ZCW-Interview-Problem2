a30424b29d92fec9b7c752b3394cc51a59f755b9
jeffrey <jeffrey@zipcoders-MacBook-Pro-3.local>
2017-11-27 11:27:31 -0500

    JLawrence Prob 2

--- src/main/java/io/zipcoder/Problem2.java
+++ src/main/java/io/zipcoder/Problem2.java
@@ -1,4 +1,20 @@
 package io.zipcoder;
 
+import java.util.Scanner;
+
 public class Problem2 {
+
+    public static void problem2(){
+
+        int usrNumber = 150;
+
+        int numToCalc = 0;
+
+        for(int x = usrNumber; x > 0; --x  ){
+            numToCalc +=x;
+        }
+    }
+
+
 }
+
