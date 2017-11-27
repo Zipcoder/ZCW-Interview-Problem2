f6ace090ff989d82a0e87aad840386ed078610e9
raul <raul@zipcoders-MacBook-Pro.local>
2017-11-27 11:10:11 -0500

    raul

--- src/main/java/io/zipcoder/Problem2.java
+++ src/main/java/io/zipcoder/Problem2.java
@@ -1,4 +1,23 @@
 package io.zipcoder;
 
 public class Problem2 {
+
+    public static void main(String args[]) {
+        fibonacciLoop(10);
+    }
+
+    private static void fibonacciLoop(int number) {
+
+        for (int i = 1; i <= number; i++) {
+
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    private static int fibonacci(int number) {
+        if (number == 1 || number == 2) {
+            return 1;
+        }
+        return fibonacci(number - 1) + fibonacci(number - 2);
+    }
 }
