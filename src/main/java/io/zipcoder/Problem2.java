815d8009fa20ff247160e454547f78e2f495a6d9
gabriela <gabriela@zipcoders-MacBook-Pro-6.local>
2017-11-27 11:18:04 -0500

    Problem 2.....

--- src/main/java/io/zipcoder/Problem2.java
+++ src/main/java/io/zipcoder/Problem2.java
@@ -1,4 +1,37 @@
 package io.zipcoder;
 
+import java.util.StringTokenizer;
+
+//complies, but test is not passing....
+
 public class Problem2 {
+
+    int numPrevious = 0;
+    int numPrePrevious = 1;
+    StringBuilder fibonacciString = new StringBuilder();
+
+    //fibonacciIteration
+    public String fibonacciIteration(int n) {
+        if (n == 0) {
+            fibonacciString.append("0");
+        }
+        else if (n == 1) {
+            fibonacciString.append("0 1");
+        }
+        else {
+            for (int i = 1; i < n; i++) {
+                int nextNumber = numPrevious + numPrePrevious;
+                fibonacciString.append(nextNumber + " ");
+                numPrevious = numPrePrevious;
+                numPrePrevious = nextNumber;
+            }
+        }
+        return fibonacciString.toString();
+    }
+
+    
+    public String fibonacciRecursion(int n) {
+
+    }
+
 }
--- src/test/java/io/zipcoder/Problem2Test.java
+++ src/test/java/io/zipcoder/Problem2Test.java
@@ -1,4 +1,21 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem2Test {
+
+    @Test
+    public void fibonacciIterationTest() {
+        Problem2 problem2Test = new Problem2();
+        String expected = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144";
+        String actual = problem2Test.fibonacciIteration(150);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fibonacciRecursionTest() {
+
+    }
+
 }
