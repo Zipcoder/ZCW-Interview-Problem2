20a2c15ae9ddf142c02d1c85f4611317192969c3
Zachary <zstimmel@gmail.com>
2017-11-27 12:02:31 -0500

    Prob2

--- src/test/java/io/zipcoder/Problem2Test.java
+++ src/test/java/io/zipcoder/Problem2Test.java
@@ -10,7 +10,7 @@ public class Problem2Test {
     public void fibonacciIterationTest(){
         String expected = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144";
 
-        String actual = test.fibonacciIteration(150);
+        String actual = test.fibon(150);
         //actual = 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 
         Assert.assertEquals(expected,actual);
