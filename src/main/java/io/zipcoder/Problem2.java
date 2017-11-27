7ce834740b330341a78dd7630626f4c962c312d7
vinceparis19 <vinceparis19@gmail.com>
2017-11-27 11:00:44 -0500

    need to work this out more, lacks flair

--- src/main/java/io/zipcoder/Problem2.java
+++ src/main/java/io/zipcoder/Problem2.java
@@ -1,4 +1,44 @@
 package io.zipcoder;
 
 public class Problem2 {
+
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(50));
+        System.out.println(fibonacci2(50));
+    }
+
+
+    public static int fibonacci(int number) {
+
+        if (number == 1 || number == 2) {
+
+            return 1;
+
+        }
+
+        return fibonacci(number - 1) + fibonacci(number - 2);
+
+    }
+
+    public static int fibonacci2(int number) {
+
+        if (number == 1 || number == 2) {
+            return 1;
+        }
+
+        int fibo1 = 1, fibo2 = 1, fibonacci = 1;
+
+        for (int i = 3; i <= number; i++) {
+            fibonacci = fibo1 + fibo2;
+            fibo1 = fibo2;
+            fibo2 = fibonacci;
+
+        }
+
+        return fibonacci;
+
+    }
 }
+
+
--- src/test/java/io/zipcoder/Problem2Test.java
+++ src/test/java/io/zipcoder/Problem2Test.java
@@ -1,4 +1,27 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem2Test {
+
+    Problem2 prob2 = new Problem2();
+
+    int n = 7;
+
+    @Test
+    public void fibonnaciRecursionTest(){
+        int expected = 13;
+        int actual = prob2.fibonacci(n);
+        Assert.assertEquals(expected, actual);
+        System.out.println(prob2.fibonacci(n));
+    }
+
+    @Test
+    public void fibonnaciLoopTest(){
+        int expected = 13;
+        int actual = prob2.fibonacci2(n);
+        Assert.assertEquals(expected, actual);
+        System.out.println(prob2.fibonacci2(n));
+    }
 }
