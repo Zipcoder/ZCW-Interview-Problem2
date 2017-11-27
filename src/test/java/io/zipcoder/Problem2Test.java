61aac62cdf63c9e39805485f1215bd6c34f537ff
zan <zan@zipcoders-MacBook-Pro.local>
2017-11-27 11:16:15 -0500

    almost there Zan

--- src/test/java/io/zipcoder/Problem2Test.java
+++ src/test/java/io/zipcoder/Problem2Test.java
@@ -1,4 +1,14 @@
 package io.zipcoder;
+import org.junit.*;
 
 public class Problem2Test {
+
+    @Test
+    public void fibonacciIterationTest(){
+
+    String expected = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144";
+
+
+    }
+    //Need to do my test. I should have started with this
 }
