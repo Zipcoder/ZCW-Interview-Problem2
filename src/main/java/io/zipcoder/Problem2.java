42766e491d4a9e9921630bfaf222aa08076c0d4d
ryan <ryan@zipcoders-MacBook-Pro-5.local>
2017-11-27 11:20:36 -0500

    Ryan Maiale, doesnt work

--- src/main/java/io/zipcoder/Problem2.java
+++ src/main/java/io/zipcoder/Problem2.java
@@ -1,4 +1,20 @@
 package io.zipcoder;
 
 public class Problem2 {
+    StringBuilder numberSequence = new StringBuilder();
+
+    public String fibonacciIteration(int number){
+
+        int[] fibonacciSeries = new int[number];
+        fibonacciSeries[0] = 0;
+        fibonacciSeries[1] = 1;
+
+        for (int i = 2; i < number; i++){
+            fibonacciSeries[i] = fibonacciSeries[i - 1] + fibonacciSeries [i - 2];
+            numberSequence.append(i);
+
+        }
+
+        return numberSequence.toString();
+    }//none of this works
 }
--- src/test/java/io/zipcoder/Problem2Test.java
+++ src/test/java/io/zipcoder/Problem2Test.java
@@ -1,4 +1,19 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class Problem2Test {
+
+    Problem2 problem2 = new Problem2();
+    int number = 150;
+
+
+    @Test
+    public void fibonacciIterationTest(){
+        String expected = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144";
+        String actual = problem2.fibonacciIteration(number);
+        Assert.assertEquals(expected, actual);
+    }
 }
