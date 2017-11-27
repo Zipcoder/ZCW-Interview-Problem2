39b92ecf657033f9fbd0228eb2b4a2ac100e4f8c
aaron <aaronherion111@gmail.com>
2017-11-27 11:06:41 -0500

    Finished

--- src/main/java/io/zipcoder/Problem2.java
+++ src/main/java/io/zipcoder/Problem2.java
@@ -1,4 +1,31 @@
 package io.zipcoder;
 
 public class Problem2 {
+
+
+    public static int fibonacciIteration(int n) {
+
+        if (n <= 1) {
+            return n;
+        }
+        int fib = 1;
+        int prevFib = 1;
+
+        for (int i = 2; i < n; i++) {
+            int temp = fib;
+            fib += prevFib;
+            prevFib = temp;
+        }
+        return fib;
+    }
+
+    public static int fibonacciRecursion(int n) {
+
+
+        if(n <= 1) {
+            return n;
+        }
+        return fibonacciRecursion(n-1) + fibonacciRecursion(n-2);
+
+    }
 }
--- src/test/java/io/zipcoder/Problem2Test.java
+++ src/test/java/io/zipcoder/Problem2Test.java
@@ -1,4 +1,27 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+
 public class Problem2Test {
+
+    @Test
+    public void fibonacciIteration() {
+
+        int expected = 6765;
+        int actual = Problem2.fibonacciIteration(20);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fibonacciRecursion() {
+
+        int expected = 75025;
+        int actual = Problem2.fibonacciRecursion(25);
+
+        Assert.assertEquals(expected, actual);
+
+    }
 }
