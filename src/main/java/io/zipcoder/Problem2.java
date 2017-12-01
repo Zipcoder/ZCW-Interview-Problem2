e077136c56c10f6b5a8aa5a996ccbcd4e45dca9f
Zachary <zstimmel@gmail.com>
2017-12-01 07:40:12 -0500

    prob 2 iterate

--- src/main/java/io/zipcoder/Problem2.java
+++ src/main/java/io/zipcoder/Problem2.java
@@ -4,29 +4,6 @@ import java.util.ArrayList;
 
 public class Problem2 {
 
-//    public String fibonacciIteration(int n) {
-//        StringBuilder builder = new StringBuilder();
-//        int current = 1;
-//        int prev = 1;
-//
-//        if (n <= 1) {
-//            builder.append(n);
-//        }
-//        for (int i = 2; i < n; i++) {
-//            int temp = current;
-//            builder.append(prev);
-//            current = prev;
-//            prev = temp;
-//
-//            builder.append(", ");
-//
-//            //doesnt work, just adds a bunch of 1's
-//        }
-//
-//        return builder.toString();
-//    }
-//
-
     public String fibon(int n) {
         ArrayList <Integer> arrayList = new ArrayList <Integer>();
         arrayList.add(0);
@@ -34,14 +11,14 @@ public class Problem2 {
 
         Integer lowerIndex = 0;
         Integer higherIndex = 1;
-        Integer fib;
+        Integer nextInt;
 
         do {
-            fib = arrayList.get(lowerIndex) + arrayList.get(higherIndex);
-            if (fib < n) {
+            nextInt = arrayList.get(lowerIndex) + arrayList.get(higherIndex);
+            if (nextInt > n) {
                 break;
             }
-            arrayList.add(fib);
+            arrayList.add(nextInt);
             lowerIndex++;
             higherIndex++;
         } while (true);
--- src/test/java/io/zipcoder/Problem2Test.java
+++ src/test/java/io/zipcoder/Problem2Test.java
@@ -11,7 +11,6 @@ public class Problem2Test {
         String expected = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144";
 
         String actual = test.fibon(150);
-        //actual = 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 
         Assert.assertEquals(expected,actual);
     }
