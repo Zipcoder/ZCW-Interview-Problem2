4654ddc45f344bdd18d3c04c6547475d07aa65b0
ryan <ryan@zipcoders-MacBook-Pro-5.local>
2017-11-30 23:03:44 -0500

    fixed it, both work

--- src/test/java/io/zipcoder/Problem2Test.java
+++ src/test/java/io/zipcoder/Problem2Test.java
@@ -6,14 +6,27 @@ import org.junit.Test;
 
 public class Problem2Test {
 
-    Problem2 problem2 = new Problem2();
-    int number = 150;
+    private Problem2 problem2;
+    private int number;
 
+    @Before
+    public void setUp() {
+        number = 150;
+        problem2 = new Problem2();
 
+    }
     @Test
     public void fibonacciIterationTest(){
         String expected = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144";
         String actual = problem2.fibonacciIteration(number);
         Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fibItArrayListTest(){
+        String expected = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144";
+        String actual = problem2.fibonacciIterationArrayList(number);
+        Assert.assertEquals(expected, actual);
+
     }
 }
