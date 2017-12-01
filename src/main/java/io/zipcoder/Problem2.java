3b1dec45a4996bc68407003bb6b89555f3f09b14
gabriela <gabriela@zipcoders-MacBook-Pro-6.local>
2017-12-01 12:12:44 -0500

    refactored iteration method

--- src/main/java/io/zipcoder/Problem2.java
+++ src/main/java/io/zipcoder/Problem2.java
@@ -6,7 +6,6 @@ public class Problem2 {
     int numPrePrevious = 1;
     StringBuilder fibonacciString = new StringBuilder();
 
-    //fibonacciIteration
     public String fibonacciIteration(int n) {
         if (n == 0) {
             fibonacciString.append("0");
@@ -14,18 +13,17 @@ public class Problem2 {
         if (n == 1) {
             fibonacciString.append("0, 1");
         } else {
-            fibonacciString.append("0, 1, ");
+            fibonacciString.append("0, 1");
             for (int nextNum = 0; nextNum < n; nextNum++) {
                 nextNum = numPrevious + numPrePrevious;
                 if (nextNum > n) {
                     break;
                 }
-                fibonacciString.append(nextNum + ", ");
+                fibonacciString.append(", " + nextNum);
                 numPrevious = numPrePrevious;
                 numPrePrevious = nextNum;
             }
         }
-        fibonacciString.setLength(fibonacciString.length() - 2);
         return fibonacciString.toString();
     }
     
