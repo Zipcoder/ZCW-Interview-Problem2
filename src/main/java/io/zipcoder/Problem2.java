1b41756992e9e837c53ee2ee70eaa1ec8a03e35b
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-04-15 15:55:04 -0400

    iterationcomplete

--- src/main/java/io/zipcoder/Problem2.java
+++ src/main/java/io/zipcoder/Problem2.java
@@ -1,4 +1,28 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Problem2 {
+    public String fibonacciIteration(int n) {
+
+        ArrayList<Integer> fibNums = new ArrayList<Integer>();
+        int firstNum = 0;
+        int secondNum = 1;
+
+        while (true) {
+            if (firstNum < n) {
+                fibNums.add(firstNum);
+                firstNum += secondNum;
+                secondNum = firstNum - secondNum;
+            } else {
+                break;
+            }
+        }
+        return fibNums.toString().replace("[", "").replace("]", "");
+    }
+//    public String fibonacciRecursion(int n) {
+//    }
 }
+
+
+
--- src/test/java/io/zipcoder/Problem2Test.java
+++ src/test/java/io/zipcoder/Problem2Test.java
@@ -1,4 +1,36 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class Problem2Test {
+
+    Problem2 testProb;
+
+    @Before
+    public void setup(){
+        testProb = new Problem2();
+    }
+
+    @Test
+    public void testFibonacciIteration(){
+        int n = 150;
+        String expectedOutput = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144";
+        String actualOutput = testProb.fibonacciIteration(n);
+        Assert.assertEquals(expectedOutput, actualOutput);
+    }
+
+
+//    @Test
+//    public void testFibonacciRecursion(){
+//        int n = 150;
+//        String expectedOutput = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144";
+//        String actualOutput = testProb.fibonacciRecursion(n);
+//        Assert.assertEquals(expectedOutput, actualOutput);
+//    }
 }
+
+
+
+
