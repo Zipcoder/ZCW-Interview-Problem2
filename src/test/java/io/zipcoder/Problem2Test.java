7d9776e0aaf1f3043962abed10cb37d3d71d4163
Vincent Gasbarro <vincentgasbarro@zipcoders-MacBook-Pro.local>
2018-04-15 14:47:46 -0400

    fibby fib fib

--- src/test/java/io/zipcoder/Problem2Test.java
+++ src/test/java/io/zipcoder/Problem2Test.java
@@ -1,4 +1,35 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class Problem2Test {
+
+    private Problem2 tester;
+
+    @Before
+    public void setup() {
+        tester = new Problem2();
+    }
+
+    @Test
+    public void fibIterationTest1() {
+        int input = 150;
+        String expected = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144";
+        String actual = tester.fibIteration(input);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fibRecursionTest1() {
+        int input = 150;
+        ArrayList<Integer> testList = new ArrayList<Integer>();
+        String expected = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144";
+        String actual = tester.fibRecursion(input, testList);
+        Assert.assertEquals(expected, actual);
+    }
+
 }
